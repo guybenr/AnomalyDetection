6c806e6ce1bb3b13c5dc6959b6454342a560cd8d
adi <adida2148@gmail.com>
2021-11-25 16:10:03 +0200

    piztes nahuiiiiiiiiiiiii

--- minCircle.cpp
+++ minCircle.cpp
@@ -26,28 +26,47 @@ Circle* createCircle(vector<Point> *points) {
     }
     Point a = points->at(0);
     Point b = points->at(1);
-    Point c = points->at(2);
-    float xab = a.x - b.x;
-    float xac = a.x - c.x;
-    float xca = c.x - a.x;
-    float xba = b.x - a.x;
-    float yab = a.y - b.y;
-    float yac = a.y - c.y;
-    float yca = c.y - a.y;
-    float yba = b.y - a.y;
-    float pXac = (a.x * a.x) - (c.x * c.x);
-    float pXba = (b.x * b.x) - (a.x * a.x);
-    float pYac = (a.y * a.y) - (c.y * c.y);
-    float pYba = (b.y * b.y) - (a.y * a.y);
-    float f =
-            ((pXac) * (xab) + (pYac) * (xab) + (pXba) * (xac) + (pYba) * (xac)) / (2 * ((yca) * (xab) - (yba) * (xac)));
-    float g =
-            ((pXac) * (yab) + (pYac) * (yab) + (pXba) * (yac) + (pYba) * (yac)) / (2 * ((xca) * (yab) - (xba) * (yac)));
-    // center of circle = (-g,-f)
-    float k = -(a.x * a.x) - (a.y * a.y) - 2 * g * a.x - 2 * f * a.y;
-    float radius = (g * g) + (f * f) - k;
-    // radius of circle = sqrt radius
-    Circle* newCircle = new Circle(Point(-g, -f), sqrt(radius));
+    Point m = points->at(2);
+    float x1 = a.x;
+    float y1 = a.y;
+    float x2 = b.x;
+    float y2 = b.y;
+    float x3 = m.x;
+    float y3 = m.y;
+    float x12 = x1 - x2;
+    float x13 = x1 - x3;
+    float y12 = y1 - y2;
+    float y13 = y1 - y3;
+    float y31 = y3 - y1;
+    float y21 = y2 - y1;
+    float x31 = x3 - x1;
+    float x21 = x2 - x1;
+
+    // x1^2 - x3^2
+    float sx13 = pow(x1, 2) - pow(x3, 2);
+
+    // y1^2 - y3^2
+    float sy13 = pow(y1, 2) - pow(y3, 2);
+
+    float sx21 = pow(x2, 2) - pow(x1, 2);
+    float sy21 = pow(y2, 2) - pow(y1, 2);
+
+    float f = ((sx13) * (x12) + (sy13) * (x12) + (sx21) * (x13) + (sy21) * (x13)) / (2 * ((y31) * (x12) - (y21) * (x13)));
+    float g = ((sx13) * (y12) + (sy13) * (y12) + (sx21) * (y13) + (sy21) * (y13)) / (2 * ((x31) * (y12) - (x21) * (y13)));
+
+    float c = -pow(x1, 2) - pow(y1, 2) - 2 * g * x1 - 2 * f * y1;
+
+    // eqn of circle be x^2 + y^2 + 2*g*x + 2*f*y + c = 0
+    // where centre is (h = -g, k = -f) and radius r
+    // as r^2 = h^2 + k^2 - c
+    float h = -g;
+    float k = -f;
+    float sqr_of_r = h * h + k * k - c;
+
+    // r is the radius
+    float r = sqrt(sqr_of_r);
+
+    Circle* newCircle = new Circle(Point(h, k), sqrt(r));
     return newCircle;
 }
 
