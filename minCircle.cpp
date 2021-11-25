58f157a59c6c746111e1d0d575012688776bbb37
adi <adida2148@gmail.com>
2021-11-25 13:59:49 +0200

    piztes nahuiiiiiiiiiiiii

--- minCircle.cpp
+++ minCircle.cpp
@@ -5,27 +5,27 @@
 #include "minCircle.h"
 #include "anomaly_detection_util.h"
 
-Circle createCircle(vector<Point> *points) {
+Circle* createCircle(vector<Point> *points) {
     if (points->empty()) {
         Circle newCircle(Point(0, 0), 0);
-        return newCircle;
+        return &newCircle;
     }
     if (points->size() == 1) {
         Circle newCircle(points->at(0), 0);
-        return newCircle;
+        return &newCircle;
     }
-    if (points.size() == 2) {
-        Point a = points[0];
-        Point b = points[1];
+    if (points->size() == 2) {
+        Point a = points->at(0);
+        Point b = points->at(1);
         float centerX = (a.x + b.x) / 2;
         float centerY = (a.y + b.y) / 2;
         float r = sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
         Circle newCircle(Point(centerX, centerY), r);
-        return newCircle;
+        return &newCircle;
     }
-    Point a = points[0];
-    Point b = points[1];
-    Point c = points[2];
+    Point a = points->at(0);
+    Point b = points->at(1);
+    Point c = points->at(2);
     float xab = a.x - b.x;
     float xac = a.x - c.x;
     float xca = c.x - a.x;
@@ -47,7 +47,7 @@ Circle createCircle(vector<Point> *points) {
     float radius = (g * g) + (f * f) - k;
     // radius of circle = sqrt radius
     Circle newCircle(Point(-g, -f), sqrt(radius));
-    return newCircle;
+    return &newCircle;
 }
 
 bool Circle::isInCircle(Point p) {
@@ -61,18 +61,30 @@ bool Circle::isInCircle(Point p) {
 
 Circle MinCircleRecursive(vector<Point *> *points, vector<Point> *boundary) {
     Circle *c = new Circle(Point(0, 0), 0);
+    bool flagBoundary = false, flagPoints = false;
+    Point p;
+    if (!points->empty()) {
+        p = *points->back();
+    }
     if (points->empty() || boundary->size() == 3)
-        *c = createCircle(boundary);
+        c = createCircle(boundary);
     else {
-        Point p = *points->back();
+        flagBoundary = true;
         points->pop_back();
         *c = MinCircleRecursive(points, boundary);
         if (!c->isInCircle(p)) {
+            flagPoints = true;
             boundary->push_back(p);
             *c = MinCircleRecursive(points, boundary);
         }
-        return *c;
     }
+    if (flagBoundary && !boundary->empty())
+        boundary->pop_back();
+    if (flagPoints){
+        Point* pointer = &p;
+        points->push_back(pointer);
+    }
+    return *c;
 }
 
 Circle findMinCircle(Point **points, size_t size) {
