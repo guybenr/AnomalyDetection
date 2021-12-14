6272093dec1ff54619575d52caa971a29c33764e
גיא בן רצון <guybenr@hotmail.com>
2021-12-14 16:30:50 +0200

    AnomalyDetection Version 2.0

--- CLI.cpp
+++ CLI.cpp
@@ -1,21 +1,21 @@
 #include "CLI.h"
 
 CLI::CLI(DefaultIO* dio, vector<Command*> commands) {
-    this.dio = dio;
+    this->dio = dio;
     this->commands = commands;
 }
 
 CLI::CLI(DefaultIO* dio) {
-    this.dio = dio;
-    this->commands = vector<*commands>;
+    this->dio = dio;
+    this->commands = vector<Command*>();
 }
 
 
 void CLI::printMenu() {
-    this.dio->write("Welcome to the Anomaly Detection Server.\n");
-    this.dio->write("Please choose an option:\n");
+    this->dio->write("Welcome to the Anomaly Detection Server.\n");
+    this->dio->write("Please choose an option:\n");
     int size = this->commands.size();
-    for (int i = o ; i < size ; ++i) {
+    for (int i = 0 ; i < size ; ++i) {
         this->dio->write(this->commands.at(i)->getDes());
     }
 }
