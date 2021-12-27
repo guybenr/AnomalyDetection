44526c58050cc34bb3648388c9ffc97d35a867a8
adi <adida2148@gmail.com>
2021-12-27 15:06:53 +0200

    aaaa

--- Server.cpp
+++ Server.cpp
@@ -2,7 +2,13 @@
 #include "Server.h"
 
 Server::Server(int port)throw (const char*) {
-
+    int sockfd = socket(domain, type, protocol);
+    int setsockopt(int sockfd, int level, int optname,
+                   const void *optval, socklen_t optlen);
+    int bind(int sockfd, const struct sockaddr *addr,
+             socklen_t addrlen);
+    int listen(int sockfd, int backlog);
+    int new_socket= accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
 }
 
 void Server::start(ClientHandler& ch)throw(const char*){
